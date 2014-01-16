62f6feff67183b273ed14ca845438508a7f5f5f2
Davor Cubranic <cubranic@stat.ubc.ca>
2014-01-16 10:47:00 -0800

    Check in the module skeleton created with module-starter
    Command used:
    
      module-starter \
        --author="Davor Cubranic" --email=cubranic@stat.ubc.ca \
        --module=Statistics::R::IO --mb \
        --license=gpl3 --minperl=5.012 \
        --ignore=git

---
+++ lib/Statistics/R/IO.pm
@@ -0,0 +1,121 @@
+package Statistics::R::IO;
+
+use 5.012;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Statistics::R::IO - The great new Statistics::R::IO!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Statistics::R::IO;
+
+    my $foo = Statistics::R::IO->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Davor Cubranic, C<< <cubranic at stat.ubc.ca> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-statistics-r-io at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Statistics-R-IO>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Statistics::R::IO
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Statistics-R-IO>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Statistics-R-IO>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Statistics-R-IO>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Statistics-R-IO/>
+
+=back
+
+
+=head1 AUTHOR
+
+Davor Cubranic, C<< <cubranic at stat.ubc.ca> >>
+
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 University of British Columbia.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+
+=cut
+
+1; # End of Statistics::R::IO
